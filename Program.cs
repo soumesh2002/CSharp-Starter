648e73cb1b2cbd1f7dd216aeaa37049977b0fca0
soume <soume@LAPTOP-QTG65AQK>
2022-07-31 01:29:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        LinkedList<string> collection = new LinkedList<string>();
+        collection.AddLast("Soumesh");
+        collection.AddLast("Aditya");
+        collection.AddLast("Devashish");
+        collection.AddLast("Amrit");
+
+
+        Console.WriteLine("Personal Name: ");
+
+        foreach (string str in collection)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
